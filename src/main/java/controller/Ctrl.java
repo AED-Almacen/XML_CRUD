6ad81543efe0d60115e17530338e273220df6c0f
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-02-16 18:37:11 +0000

    update controller

--- src/main/java/controller/Ctrl.java
+++ src/main/java/controller/Ctrl.java
@@ -1,12 +1,18 @@
 package controller;
 
+import model.Book;
+import model.Queries;
 import view.StacKOfBooks;
 
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class Ctrl implements ActionListener {
     private final StacKOfBooks stacKOfBooks;
+    private final Queries queries;
+
     private void windowConfig() {
         this.stacKOfBooks.setTitle("Stack of Books");
         this.stacKOfBooks.setLocationRelativeTo(null);
@@ -14,9 +20,29 @@ public class Ctrl implements ActionListener {
         this.stacKOfBooks.setVisible(true);
     }
 
+    private void createTable(List<Book> books) {
+        Object[][] data = new Object[books.size()][];
+
+        for (int i = 0; i < books.size(); i++) {
+            data[i] = new String[]{
+                    String.valueOf(books.get(i).getId()),
+                    books.get(i).getName(),
+                    String.valueOf(books.get(i).getPages())
+            };
+        }
+
+        this.stacKOfBooks.getTable().setModel(new DefaultTableModel(
+                data,
+                new String[]{"Id", "Name", "Pages"}
+        ));
+    }
+
     public Ctrl() {
         this.stacKOfBooks = new StacKOfBooks();
+        this.queries = new Queries();
         windowConfig();
+
+        this.createTable(queries.readBooks());
     }
 
     @Override
