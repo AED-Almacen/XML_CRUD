3710e32c3a857d36f0a45b8a5479712aaf7f04de
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-02-18 13:35:40 +0000

    create view

--- src/main/java/view/StacKOfBooks.java
+++ src/main/java/view/StacKOfBooks.java
@@ -4,9 +4,19 @@ import javax.swing.*;
 
 public class StacKOfBooks extends JFrame {
     private JPanel window;
-    private JScrollPane ScrollPane;
+    private JScrollPane scrollPane;
     private JTable table;
     private JPanel form;
+    private JTextField pagesText;
+    private JTextField idText;
+    private JTextField titleText;
+    private JButton addBtn;
+    private JButton dropBtn;
+    private JButton updateBtn;
+    private JPanel buttons;
+    private JLabel idLabel;
+    private JLabel titleLabel;
+    private JLabel pagesLabel;
 
 
     public StacKOfBooks() {
@@ -14,15 +24,32 @@ public class StacKOfBooks extends JFrame {
         setContentPane(window);
     }
 
-    public JPanel getWindow() {
-        return this.window;
+    public JTextField getPagesText() {
+        return this.pagesText;
+    }
+
+    public JTextField getIdText() {
+        return this.idText;
+    }
+
+    public JTextField getTitleText() {
+        return this.titleText;
+    }
+
+    public JButton getAddBtn() {
+        return this.addBtn;
+    }
+
+    public JButton getDropBtn() {
+        return this.dropBtn;
+    }
+
+    public JButton getUpdateBtn() {
+        return this.updateBtn;
     }
 
     public JTable getTable() {
         return this.table;
     }
 
-    public JScrollPane getScrollPane() {
-        return ScrollPane;
-    }
 }
